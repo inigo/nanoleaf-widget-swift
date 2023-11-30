855da4fc63cb91848a6952edd1a1446ad99a13fa
Inigo Surguy <inigo.surguy@67bricks.com>
2023-11-30 22:05:13 +0000

    Initial Commit

---
+++ nanoleaf widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nanoleaf widget
+//
+//  Created by Inigo Surguy on 30/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nanoleaf widget/nanoleaf_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanoleaf_widgetApp.swift
+//  nanoleaf widget
+//
+//  Created by Inigo Surguy on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct nanoleaf_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
